b7291b11b6f083ab8988034eec0c4612a709d748
joascampos <joascampos02@gmail.com>
2024-07-22 23:08:43 -0300

    Estudar tipos primitivos e classe String

--- tipos-variaveis/src/TiposVariaveis.java
+++ tipos-variaveis/src/TiposVariaveis.java
@@ -1,5 +1,17 @@
 public class TiposVariaveis {
     public static void main(String[] args) throws Exception {
+        double salarioMinimo = 2500;
         System.out.println("Hello, World!");
+        short numeroCurto = 1;
+        int numeroNormal = numeroCurto;
+        short numeroCurto2 = (short) numeroNormal;
+
+        double VALOR_DE_PI = 3.14;
+        VALOR_DE_PI = 10.75;
+
+        //ESTUDAR TIPOS PRIMITIVOS
+        //ESTUDAR A CLASSE STRING QUE REPRESENTA TEXTO NA APLICAÇÃO
+
+        
     }
 }
